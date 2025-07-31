4be4375a03f50c9470624bc8298b3de5c95e5b16
Nikita_Tulupov <nikita.tulupov@voiceitt.com>
2025-07-31 20:43:35 +0300

    add docs for deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
       <ToastContainer
         position="top-right"
@@ -17,6 +17,6 @@ createRoot(document.getElementById("root")!).render(
         closeOnClick
         pauseOnHover
       />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
