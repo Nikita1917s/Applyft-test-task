ee50628a03209c2d668b8c46ebef73273eb18304
Nikita_Tulupov <nikita.tulupov@voiceitt.com>
2025-07-31 23:00:38 +0300

    add docs for deploy

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById("root")!).render(
       <App />
       <ToastContainer
         position="top-right"
-        autoClose={5000}
+        autoClose={5001}
         hideProgressBar={false}
         newestOnTop
         closeOnClick
