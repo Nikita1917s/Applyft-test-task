4a1e15661911d2515441fb853a41c8c2a183a0a9
Nikita_Tulupov <nikita.tulupov@voiceitt.com>
2025-07-31 23:11:43 +0300

    add docs for deploy

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <App />
       <ToastContainer
         position="top-right"
